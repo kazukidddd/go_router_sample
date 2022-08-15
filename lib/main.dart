0da220538b7706e8ac09e8f611567db2439cc38c
kazukidddd <kazukidddd836@gmail.com>
2022-08-15 21:20:56 +0900

    routerをmainに反映

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_sample/screena.dart';
-import 'package:go_router_sample/screenb.dart';
-import 'package:go_router_sample/screenc.dart';
+import 'package:go_router_sample/router.dart';
 
 void main() {
   runApp(
@@ -14,30 +12,8 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   final GoRouter _router = GoRouter(
     debugLogDiagnostics: true,
-    routes: <GoRoute>[
-      GoRoute(
-        path: '/',
-        builder: (BuildContext context, GoRouterState state) {
-          return const ScreenA();
-        },
-        routes: <GoRoute>[
-          GoRoute(
-            path: 'b',
-            builder: (BuildContext context, GoRouterState state) {
-              return const ScreenB();
-            },
-            routes: <GoRoute>[
-              GoRoute(
-                path: 'c',
-                builder: (BuildContext context, GoRouterState state) {
-                  return const ScreenC();
-                },
-              )
-            ],
-          ),
-        ],
-      ),
-    ],
+    initialLocation: '/',
+    routes: $appRoutes,
   );
 
   @override
